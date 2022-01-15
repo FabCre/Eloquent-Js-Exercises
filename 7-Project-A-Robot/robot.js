8d31cf5fd768bbe1cad6fb93c8cc5ffb9a072d67
FabCre <fabien@await.fr>
2022-01-15 15:28:26 +0100

    exercices 7.2 & 7.3 & 8.1 & 8.2

--- 7-Project-A-Robot/robot.js
+++ 7-Project-A-Robot/robot.js
@@ -1,4 +1,4 @@
-var roads = [
+﻿var roads = [
     "Alice's House-Bob's House",   "Alice's House-Cabin",
     "Alice's House-Post Office",   "Bob's House-Town Hall",
     "Daria's House-Ernie's House", "Daria's House-Town Hall",
